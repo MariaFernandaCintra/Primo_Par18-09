0bc27770393323a42160711cf36ec7c05d1c36d4
MariaFernandaCintra <mariafernandaccintra@gmail.com>
2024-09-18 13:02:35 -0300

    18-09

--- src/controllers/ControllerNumber.js
+++ src/controllers/ControllerNumber.js
@@ -1,6 +1,11 @@
 module.exports = class ControllerNumber{
     static async checkPar(req, res){
         const{numero} = req.body;
+
+        if(typeof numero !== "number"){
+            return res.status(400).json({message: "Não é um número!!"})
+        }
+
         //verifica se o número é par de forma mais limpa e fácil
         if(numero % 2 == 0){
             return res.status(200).json({message:"Número Par!"});
@@ -9,6 +14,10 @@ module.exports = class ControllerNumber{
     }
     static async checkPrimo(req, res){
         const {numero} = req.body;
+        
+        if(typeof numero !== "number"){
+            return res.status(400).json({message: "Não é um número!!"})
+        }
 
         if (numero <= 1){
             return res.status(200).json({message: "O número 1 não é primo"});
